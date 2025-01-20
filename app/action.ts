6f290ea63a5013fba1b0b99a98ea1e4f696a3eac
kiyopikko <piyo326@gmail.com>
2025-01-20 22:10:42 +0900

    revert: wait 5000ms after invalidation

--- app/action.ts
+++ app/action.ts
@@ -2,15 +2,11 @@
 
 import { cookies } from 'next/headers';
 
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-
 export const createTrunsaction = async () => {
   const cookieStore = await cookies();
   // NOTE: invalidate router cache by setting cookie
   // https://nextjs.org/docs/app/building-your-application/caching#invalidation-1
   cookieStore.set('id', '1234');
-
-  await sleep(5000);
 
   return {
     url: 'https://example.com',
